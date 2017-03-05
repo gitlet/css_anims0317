f03e291d8671b667f60840942a82f2a7768be745
gitlet <11thdim@gmail.com>
2017-03-05 20:24:54 +0530

    First commit, uploading files

---
+++ assets/scripts/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+  // function mouseEnter() {
+  //   setInterval(function() {
+  //     addClass('mouseenter-class');
+  //   });
+  // }
+  //
+  // function mouseLeave() {
+  //   setInterval(function() {
+  //     addClass('mouseleave-class');
+  //   });
+  // }
+
+  // .............. works! ....................
+  // $('body .wrapper').mouseenter(function() {
+  //   $(this).addClass('mouseenter-class');
+  // });
+  //
+  // $('body .wrapper').mouseleave(function() {
+  //   $(this).addClass('mouseleave-class');
+  // });
+});
